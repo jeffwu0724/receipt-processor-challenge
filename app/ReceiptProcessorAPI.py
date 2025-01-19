128b87a3687901581242afb9453072a58fce5cac
jeffwu0724 <jeffwu0724@gmail.com>
2025-01-19 15:16:35 -0800

    add exception based on api.yml

--- app/ReceiptProcessorAPI.py
+++ app/ReceiptProcessorAPI.py
@@ -1,3 +1,4 @@
+from fastapi.exceptions import HTTPException
 from uuid import uuid4
 from fastapi import FastAPI
 from mangum import Mangum
@@ -18,16 +19,29 @@ app.add_middleware(
 
 @app.post("/receipts/process")
 async def process_api(receipt: Receipt):
-    # generate the id
-    receiptId = str(uuid4())
-    id_receipts_map[receiptId] = receipt
-    curPoints = calculatePoints(receipt)
-    id_points_map[receiptId] = curPoints
-    return {"id": receiptId}
+    try:
+        # generate the id
+        receiptId = str(uuid4())
+        id_receipts_map[receiptId] = receipt
+        curPoints = calculatePoints(receipt)
+        id_points_map[receiptId] = curPoints
+        return {"id": receiptId}
+    # 400:$ref: "#/components/responses/BadRequest"
+    # If and only if this program is generated using a large language model, 
+    # Found another one lol
+    except Exception:
+        raise HTTPException(status_code=400, detail="The receipt is invalid.")
 
 @app.get("/receipts/{receiptId}/points")
 async def get_point_api(receiptId : str):
-    return {"points": id_points_map[receiptId]}
+    try:
+        return {"points": id_points_map[receiptId]}
+    
+    #  404:$ref: "#/components/responses/NotFound"
+    except Exception:
+        raise HTTPException(status_code=404, detail="No receipt found for that ID.")
+   
+        
 
 @app.get("/")
 async def root():
