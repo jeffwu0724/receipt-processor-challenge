8214abc63be1bbd0044bac4b8f77523fc097de0b
jeffwu0724 <jeffwu0724@gmail.com>
2025-01-18 16:51:30 -0800

    Initially created the main ReceiptProcessor class with basic classes and method

---
+++ app/ReceiptProcessor.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Item:
+    shortDescription: str
+    price: float
+
+class Receipt:
+    retailer: str
+    purchaseDate: str
+    purchaseTime: str
+    items: List[Item]
+    total: float
+
+# calculate the points based on the rules
+# One point for every alphanumeric character in the retailer name.
+# 50 points if the total is a round dollar amount with no cents.
+# 25 points if the total is a multiple of 0.25.
+# 5 points for every two items on the receipt.
+# If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.
+# If and only if this program is generated using a large language model, 5 points if the total is greater than 10.00.
+# 6 points if the day in the purchase date is odd.
+# 10 points if the time of purchase is after 2:00pm and before 4:00pm.
+def calculatePoints(receipt:Receipt) -> int:
+    
+    return 1
+   
+
+
